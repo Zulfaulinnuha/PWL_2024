7a375881c50460ce8897cf83633f0128026d9819
= <=>
2024-08-28 15:23:49 +0700

    Data Diri

---
+++ resources/views/datadiri.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>BIODATA DIRI</h1><br>
+        <div class="content">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>: Zulfa Ulinnuha Azazi</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: 2241760131</td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>: SIB 3A</td>
+            </tr>
+            <tr>
+                <td>Prodi</td>
+                <td>: D4 Sistem Informasi Bisnis</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>: Teknologi Informasi</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tgl Lahir</td>
+                <td>: Batam, 14 Juli 2003</td>
+            </tr>
+            <tr>
+                <td>Alamat </td>
+                <td>: Tlogo, Blitar</td>
+            </tr>
+            <tr>
+                <td colspan=4 style="padding-bottom: 50px;"></td>
+            </tr>
+        </table>        
+        </div>
+</body>
+</html>
